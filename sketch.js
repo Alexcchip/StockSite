fc965c25eeac943f397d6645f47ba9612710ef3b
Alex Weinberger <alexcchip@gmail.com>
2022-02-22 01:09:22 -0500

    Update sketch.js
    Creds/Display bug fixes

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ let input;
 function setup() {
   createCanvas(1920, 1080);
   background(236, 227, 252);
-  textSize(55);
+  textSize(70);
   textAlign(CENTER, CENTER);
   inp = createInput('Enter Ticker (press enter to select)');
   inp.position((width / 2)-85, (height / 2)-200);
@@ -22,6 +22,10 @@ function setup() {
 function inpTicker() {
   ticker = inp.value()
   console.log(ticker)
+  clearScr()
+  textSize(55)
+  textAlign(CENTER,CENTER)
+  text(`${ticker}`, width / 2, height / 2);
 }
 function draw() {
   textSize(25)
@@ -47,7 +51,11 @@ function clearScr() {
   textSize(55)
   textAlign(CENTER,CENTER)
   text(`${ticker}`, width / 2, height / 2);
-  
+  textSize(25)
+  text(`Created by: Alexander Weinberger
+ Contact: aw700m@bhsec.bard.edu`, width / 4, (height * .65));
+  textSize(70)
+  noLoop()
 }
 /*function myInputEvent() {
   console.log(this.value());
@@ -81,7 +89,7 @@ function gotDataHigh(data) {
   else {
     set = true
   }
-  let new_data = text(`${data.high}`, width / 2, height / 2 + 100);
+  let new_data = text(`$${data.high}`, width / 2, height / 2 + 100);
   console.log(`${data.high} testing`)
 }
 
@@ -95,6 +103,6 @@ function gotDataLow(data) {
   else {
     set = true
   }
-  let new_data = text(`${data.low}`, width / 2, height / 2 + 100);
+  let new_data = text(`$${data.low}`, width / 2, height / 2 + 100);
   console.log("test")
 }
