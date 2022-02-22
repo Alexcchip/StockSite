915753fbf80d40102bbaf3bfc479321b1daf2d2e
Alex Weinberger <alexcchip@gmail.com>
2022-02-22 00:26:52 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,38 +1,82 @@
 let url = "https://api.polygon.io/v1/open-close/";
-let ticker = "AAPL";
-let yea = "/2021";
-let mo = "-06-";
+let ticker = "NFLX";
+let yea = "/2022";
+let mo = "-02-";
 let da = "18";
 let url2 = "?adjusted=true&apiKey=KVV51cHIUAc27CoerjSUKsaQX6o6IIxD";
 let link;
-let dataChoice = close;
+
+
 function setup() {
   createCanvas(1920, 1080);
   background(236, 227, 252);
   textSize(55);
   textAlign(CENTER, CENTER);
+  /*let inp = createInput('');
+  inp.position(0, 0);
+  inp.size(100);
+  inp.input(myInputEvent);*/
+  
 }
 
 function draw() {
   text(`${ticker}`, width / 2, height / 2);
-  noLoop()
+  clearButton = createButton("Clear");
+  clearButton.position((width / 2)+100, (height / 2)-100)
+  clearButton.mousePressed(clearScr)
+  highButton = createButton('High')
+  highButton.position(width / 2, (height / 2)-100)
+  highButton.mousePressed(high)
+  lowButton = createButton('Low')
+  lowButton.position((width / 2)-100, (height / 2)-100)
+  lowButton.mousePressed(low)
+
+  noLoop();
+  
 }
 
-function mousePressed() {
-  d = dist(mouseX, mouseY, 960, 540);
-  if (d < 50) {
-    findStock();
-  }
+function clearScr() {
+  background(236, 227, 252)
+  console.log("test")
+  textSize(55)
+  textAlign(CENTER,CENTER)
+  text(`${ticker}`, width / 2, height / 2);
+  
+}
+/*function myInputEvent() {
+  console.log(this.value());
+  ticker = this.value()
+  console.log(`The ticker is${ticker}`)
+  noLoop();
+}*/
+function high() {
+  findStockHigh();
 }
 
-function findStock() {
-  loadJSON(url + ticker + yea + mo + da + url2, gotData);
+function low() {
+  findStockLow();
 }
 
-function gotData(data) {
+function findStockHigh() {
+  loadJSON(url + ticker + yea + mo + da + url2, gotDataHigh);
+}
+
+function findStockLow() {
+  loadJSON(url + ticker + yea + mo + da + url2, gotDataLow);
+}
+
+function gotDataHigh(data) {
   console.log(data.high);
   textAlign(CENTER, BOTTOM);
   textSize(70);
   let new_data = text(`${data.high}`, width / 2, height / 2 + 100);
+  console.log("test")
+}
+
+function gotDataLow(data) {
+  console.log(data.high);
+  textAlign(CENTER, BOTTOM);
+  textSize(70);
+  let new_data = text(`${data.low}`, width / 2, height / 2 + 100);
   console.log("test")
 }
