1549d918ba5a4b888c4e1197070bbf1d5245a867
Alex Weinberger <alexcchip@gmail.com>
2022-02-21 20:07:00 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,47 @@
 let url = "https://api.polygon.io/v1/open-close/"
-let ticker = "PYPL"
+let ticker = "AAPL"
+let yea = "/2021"
+let mo = "-06-"
+let da = "18"
+let url2 = "?adjusted=true&apiKey=KVV51cHIUAc27CoerjSUKsaQX6o6IIxD"
+let link;
+let dataChoice = close
+function setup() {
+  createCanvas(400,400);
+  background(0,150,175)
+  textSize(55)
+  textAlign(CENTER,CENTER)
+  
+  
+  
+}
+
+function draw() {
+  text(`${ticker}`,width/2,height/2)
+  
+  
+}
+
+function mousePressed(){
+  d = dist(mouseX,mouseY,200,200)
+  if (d < 50) {
+    findStock()
+    
+  }
+}
+
+function findStock() {
+  loadJSON(url + ticker + yea + mo + da + url2, gotData)
+}
+
+function gotData(data) {
+  console.log(data.high);
+  textAlign(CENTER,BOTTOM)
+  textSize(70)
+  let new_data = text(`${data.high}`,(width/2),(height/2)+100)
+  
+}let url = "https://api.polygon.io/v1/open-close/"
+let ticker = "AAPL"
 let yea = "/2021"
 let mo = "-06-"
 let da = "18"
